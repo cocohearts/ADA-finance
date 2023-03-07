8e95e2e895a11be06a6b768e527a70fa477d04f1
ALex Zhao <alex.zhao.superman@gmail.com>
2023-03-07 15:45:26 -0800

    added some new vertex GCP stuff, hopef i can get it to work eventually

---
+++ Vertex/predict.py
@@ -0,0 +1,69 @@
+from kfp.dsl import pipeline
+from kfp.v2 import compiler
+from kfp.v2.dsl import component
+from kfp.v2.google.client import AIPlatformClient
+from train_load import project_id, gcs_bucket, region, train_pipeline_name, predict_pipeline_name, pipeline_root_path, model_name
+
+# # TODO: Change with your project id and gcs bucket name
+# project_id = "packt-data-eng-on-gcp"
+# gcs_bucket = "packt-data-eng-on-gcp-vertex-ai-pipeline"
+# region = "us-central1"
+# predict_pipeline_name = "ai-pipeline-credit-default-predict"
+# train_pipeline_name = "ai-pipeline-credit-default-train"
+# pipeline_root_path = f"gs://{gcs_bucket}/{predict_pipeline_name}"
+
+# model_name = "cc_default_rf_model.joblib"
+
+@component(packages_to_install=["google-cloud-storage","pandas","scikit-learn==0.21.3","fsspec","gcsfs"])
+def predict_batch(gcs_bucket: str, predict_file_path: str, model_path: str, output_path: str):
+    from sklearn.externals import joblib
+    from google.cloud import storage
+    import pandas as pd
+
+    project_id = project_id
+    model_local_uri = model_name
+    gcs_client = storage.Client(project=project_id)
+    bucket = gcs_client.get_bucket(gcs_bucket)
+
+    # Load predict data from GCS to pandas
+    dataframe = pd.read_csv(f'gs://{gcs_bucket}/{predict_file_path}')
+
+    # Load ML model from GCS
+    model_file = bucket.blob(model_path)
+    model_file.download_to_filename(model_local_uri)
+    loaded_model = joblib.load(model_local_uri)
+
+    # Predict
+    prediction = loaded_model.predict(dataframe)
+    prediction = pd.DataFrame(prediction)
+
+    # Store prediction to GCS
+    bucket.blob(output_path).upload_from_string(prediction.to_csv(index=False), 'text/csv')
+
+    print(f"Prediction file path: {output_path}")
+
+@pipeline(
+    name="predictionpipeline",
+    description="Prediction Pipeline",
+    pipeline_root=pipeline_root_path,
+)
+def pipeline():
+    # TODO
+    predict_file_path = ""
+    predict_batch(
+        gcs_bucket,
+        predict_file_path,
+        f"{train_pipeline_name}/artefacts/{model_name}",
+        f"{predict_pipeline_name}/artefacts/prediction.csv"
+    )
+
+compiler.Compiler().compile(
+    pipeline_func=pipeline, package_path=f"{predict_pipeline_name}.json"
+)
+
+api_client = AIPlatformClient(project_id=project_id, region=region)
+
+response = api_client.create_run_from_job_spec(
+    job_spec_path=f"{predict_pipeline_name}.json",
+    pipeline_root=pipeline_root_path
+)
---
+++ Vertex/train_load.py
@@ -0,0 +1,86 @@
+from kfp.dsl import pipeline
+from kfp.v2 import compiler
+from kfp.v2.dsl import component
+from kfp.v2.google.client import AIPlatformClient
+import pandas as pd
+
+import sys
+sys.path.append('..')
+import neural_net
+
+# TODO: Change with your project id and gcs bucket name
+project_id = "packt-data-eng-on-gcp"
+gcs_bucket = "packt-data-eng-on-gcp-vertex-ai-pipeline"
+region = "us-central1"
+train_pipeline_name = "ai-pipeline-credit-default-train"
+pipeline_root_path = f"gs://{gcs_bucket}/{train_pipeline_name}"
+
+model_name = "cc_default_rf_model.joblib"
+
+@component(packages_to_install=["google-cloud-bigquery","google-cloud-storage","pandas","pyarrow"])
+def gcs_load_data(output_gcs_bucket: str) -> str:
+    from google.cloud import storage
+
+    output_file = f"{train_pipeline_name}/artefacts/train.csv"
+
+    dataframe = pd.DataFrame()
+
+    # this line might be issue, not sure
+    gcs_client = storage.Client(project=project_id)
+
+    bucket = gcs_client.get_bucket(output_gcs_bucket)
+    bucket.blob(output_file).upload_from_string(dataframe.to_csv(index=False), 'text/csv')
+
+    return output_file
+
+@component(packages_to_install=["google-cloud-storage","pandas","scikit-learn==0.21.3","fsspec","gcsfs"])
+def train_model(gcs_bucket: str, train_file_path: str, model_name: str):
+    from google.cloud import storage
+    from sklearn.model_selection import train_test_split
+    from sklearn import metrics
+    from sklearn.externals import joblib
+    import pandas as pd
+
+    dataframe = pd.read_csv(f'gs://{gcs_bucket}/{train_file_path}')
+
+    output_file = f"ai-pipeline-credit-default-train/artefacts/{model_name}"
+
+    # TODO: GET STUFF FROM dataframe
+    x_train = dataframe[0]
+    y_train = dataframe[1]
+    x_test = dataframe[2]
+    y_test = dataframe[3]
+
+    my_model = neural_net.train(x_train,y_train)
+
+    y_pred = neural_net.test(my_model,x_test,y_test)
+
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+    joblib.dump(my_model, model_name)
+
+    bucket = storage.Client().bucket(gcs_bucket)
+    blob = bucket.blob(output_file)
+    blob.upload_from_filename(model_name)
+
+    print(f"Model saved in : {output_file}")
+
+@pipeline(
+    name="trainingpipeline",
+    description="Training Pipeline",
+    pipeline_root=pipeline_root_path,
+)
+def pipeline():
+    load_output = gcs_load_data(gcs_bucket)
+    train_model(gcs_bucket, load_output.output, model_name)
+
+compiler.Compiler().compile(
+    pipeline_func=pipeline, package_path=f"{train_pipeline_name}.json"
+)
+
+api_client = AIPlatformClient(project_id=project_id, region=region)
+
+response = api_client.create_run_from_job_spec(
+    job_spec_path=f"{train_pipeline_name}.json",
+    pipeline_root=pipeline_root_path
+)
