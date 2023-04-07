0cf28bc24b5b56913866ee4a2e711b4f76c07d96
Daniel Wang <DanielW25@lakesideschool.org>
2023-04-06 21:20:40 -0700

    :hammer: Pickled companies array to negate the need for API calls during demo

--- stockscreener.py
+++ stockscreener.py
@@ -3,13 +3,15 @@ from stock import Stock
 import finnhub
 import pandas as pd
 import time
+import pickle
 
-
-criteria = {'PE': ('<', 10), 'PB': ('<=', 0.7), 'RG5Y': ('>=', 10), 'PS': ('<', 1), 'PM5Y': ('>', 30),
-            'ROAE': ('>', 10), 'DE': ('<', 50), 'CR': ('>', 1.5)}
+# criteria = {'PE': ('<', 10), 'PB': ('<', 0.7), 'RG5Y': ('>', 10), 'PS': ('<', 1), 'PM5Y': ('>', 30),
+#             'ROAE': ('>', 10), 'DE': ('<', 50), 'CR': ('>', 1.5)}
+criteria = {'PE': ('<', 10), 'PB': ('<', 0.7)}
 translations = {'PE': 'peNormalizedAnnual', 'PB': 'pbAnnual', 'RG5Y': 'revenueGrowth5Y', 'PS': 'psTTM',
                 'PM5Y': 'netProfitMargin5Y', 'ROAE': 'roae5Y', 'DE': 'totalDebt/totalEquityAnnual',
                 'CR': 'currentRatioAnnual'}
+finnhub_client = finnhub.Client(api_key="cdq10f2ad3i5u3ridjs0cdq10f2ad3i5u3ridjsg")
 
 
 def get_metrics(c):
@@ -38,26 +40,31 @@ def find_matches(stocks, criteria):
                 profile = finnhub_client.company_profile2(symbol=symbol)
 
             c.industry = profile["finnhubIndustry"]
-            c.market_cap = profile["marketCapitalization"]
+            c.market_cap = round(profile["marketCapitalization"], 2)
 
             try:
-                c.price = finnhub_client.quote(symbol=symbol)
+                c.price = finnhub_client.quote(symbol=symbol)["c"]
             except finnhub.FinnhubAPIException:
                 print("sleeping")
                 time.sleep(60)
-                c.price = finnhub_client.company_profile2
+                c.price = finnhub_client.company_profile2(symbol=symbol)["c"]
 
             match.append(c)
     return match
 
 
-finnhub_client = finnhub.Client(api_key="cdq10f2ad3i5u3ridjs0cdq10f2ad3i5u3ridjsg")
-sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0][:10]
+"""
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 companies = [Stock(symbol=sp500["Symbol"][i], name=sp500["Security"][i]) for i in range(len(sp500))]
 for i in range(len(companies)):
+    if i % 10 == 0:
+        print(f"Getting metrics for {i}th company")
     companies[i] = get_metrics(companies[i])
-# print(companies)
+print("Done getting metrics")
 
+pickle.dump(companies, open("companies.p", "wb"))"""
+
+companies = pickle.load(open("companies.p", "rb"))
 matches = find_matches(companies, criteria)
-# for c in matches:
-#   print(c.symbol, c.name, c.metrics, c.industry, c.market_cap, c.price)
+for c in matches:
+    print(c.symbol, c.name, c.metrics, c.industry, c.market_cap, c.price)
--- stockscreener_functions.py
+++ stockscreener_functions.py
@@ -4,16 +4,19 @@ import pandas as pd
 
 def insert_metrics(data_dict: dict, criteria: dict, translations: dict):
     stock_dict = dict()
-    try:
-        for c in criteria.keys():
+    for c in criteria.keys():
+        try:
             stock_dict[c] = data_dict['metric'][translations[c]]
-    except KeyError:
-        pass
+        except KeyError:
+            pass
     return stock_dict
 
 
 def match_conditions(criteria: dict, metrics: dict):
-    for m, v in metrics.items():
+    for m in criteria.keys():
+        if m not in metrics.keys():
+            return False
+        v = metrics[m]
         (c, t) = criteria[m]
         if type(v) in (int, float):
             if c == '>':
@@ -21,12 +24,6 @@ def match_conditions(criteria: dict, metrics: dict):
                     return False
             elif c == '<':
                 if not v < t:
-                    return False
-            elif c == '>=':
-                if not v >= t:
-                    return False
-            elif c == '<=':
-                if not v <= t:
                     return False
     return True
 
