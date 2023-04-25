25ba95e3efd007b4f7c132a2ff36d9600822c174
ALex Zhao <alex.zhao.superman@gmail.com>
2023-04-25 10:18:41 -0700

    jinja for screener

--- Website/app.py
+++ Website/app.py
@@ -1,7 +1,8 @@
 from re import template
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_crontab import Crontab
 from Vertex.predict import *
+from StockScreener.load_metrics import *
 
 app = Flask(__name__)
 crontab = Crontab(app)
@@ -16,7 +17,21 @@ def about():
 
 @app.route('/screener')
 def screener():
-    return render_template('screener.html.j2')
+    return render_template('screener.html.j2',items = translations.keys(), translations = translations)
+
+@app.route('/screener_results')
+def screener_results():
+    criteria = {}
+    for item in translations:
+        item_value = request.values.get(item+"_val")
+        item_dir = request.values.get(item+"_dir")
+        criteria[item] = (item_dir,item_value)
+    
+    companies = pickle.load(open("../StockScreener/companies.p", "rb"))
+
+    matches = find_matches(companies, criteria)
+
+    return render_template('screener_results.html.j2',items = translations.keys(), translations = translations, matches=matches)
 
 @app.route('/forecast')
 def forecast():
@@ -25,4 +40,7 @@ def forecast():
 @crontab.job(minute="0", hour="0")
 def my_scheduled_job():
     exec(open("../Vertex/pipeline_setup.py").read())
+    # for prediction
     call_model()
+    # for stockscreener
+    load_metrics()
