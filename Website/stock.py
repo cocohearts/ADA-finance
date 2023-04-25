11d7c5990fd3d27b6ddc5deed09510585d7b5fef
ALex Zhao <alex.zhao.superman@gmail.com>
2023-04-25 08:58:46 -0700

    deleted two files

--- Website/stock.py
+++
@@ -1,9 +0,0 @@
-class Stock:
-    def __init__(self, symbol='', name='', c_price=0, exchange='', industry='', weburl='', market_cap=0):
-        self.symbol = symbol
-        self.name = name
-        self.c_price = c_price
-        self.exchange = exchange
-        self.industry = industry
-        self.web_url = weburl
-        self.market_cap = market_cap
